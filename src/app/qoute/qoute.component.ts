8fa8492383b306850343514042e8d0ff6ba66d8f
Leina33 <dominickrutto@gmail.com>
2019-10-28 19:18:59 +0300

    Adding show and hide logic in toggle deatils button

--- src/app/qoute/qoute.component.ts
+++ src/app/qoute/qoute.component.ts
@@ -1,24 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import {Qoutes} from '../qoutes';
+import { Component, OnInit } from "@angular/core";
+import { Qoutes } from "../qoutes";
 
 @Component({
-  selector: 'app-qoute',
-  templateUrl: './qoute.component.html',
-  styleUrls: ['./qoute.component.css']
+  selector: "app-qoute",
+  templateUrl: "./qoute.component.html",
+  styleUrls: ["./qoute.component.css"]
 })
 export class QouteComponent implements OnInit {
-
-  qoutes:Qoutes []  = [
-    new Qoutes(1, 'Martin Luther King', 'No man is an Island'),
-    new Qoutes(2,'Barack Obama','Yes you you can'),
-    new Qoutes(3,'Joel Osteen','Believe in you can do it!'),
-    new Qoutes(4,'Unknown','Karma is a bitch'),
-    new Qoutes(5,'Activists','You can people but you cannot finish ideas'),
-    
-  ]
-  constructor() { }
-
-  ngOnInit() {
+  qoutes: Qoutes[] = [
+    new Qoutes(1, "Martin Luther King", "No man is an Island"),
+    new Qoutes(2, "Barack Obama", "Yes you you can"),
+    new Qoutes(3, "Joel Osteen", "Believe in you can do it!"),
+    new Qoutes(4, "Unknown", "Karma is a bitch"),
+    new Qoutes(5, "Activists", "You can people but you cannot finish ideas")
+  ];
+  toggleDetails(index) {
+    this.qoutes[index].showDescription = !this.qoutes[index].showDescription;
   }
+  constructor() {}
 
+  ngOnInit() {}
 }
