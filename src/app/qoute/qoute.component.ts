a8506db7f73e536216a8f5de6804eb413fe0cea8
Leina33 <dominickrutto@gmail.com>
2019-10-28 17:45:58 +0300

    This component nest the app qoute

--- src/app/qoute/qoute.component.ts
+++ src/app/qoute/qoute.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Qoutes} from '../qoutes';
 
 @Component({
   selector: 'app-qoute',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./qoute.component.css']
 })
 export class QouteComponent implements OnInit {
+
+  qoutes:Qoutes []  = [
+    {id:1, name:'No man is an Island'},
+    {id:2,name:'The beauty of life is to love your self'},
+    {id:3,name:'Karma is a bitch'},
+    {id:4,name:'Choosing to be positive and having a grateful attitude is going to determine how you are going to live your life.'}
+  ];
 
   constructor() { }
 
