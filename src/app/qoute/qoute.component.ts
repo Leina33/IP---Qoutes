03bbd4fde054244f80e889a4e32866a924cd32bf
Leina33 <dominickrutto@gmail.com>
2019-10-29 07:39:41 +0300

    This code sets the date to the html file

--- src/app/qoute/qoute.component.ts
+++ src/app/qoute/qoute.component.ts
@@ -8,11 +8,11 @@ import { Qoutes } from "../qoutes";
 })
 export class QouteComponent implements OnInit {
   qoutes: Qoutes[] = [
-    new Qoutes(1, "Martin Luther King", "No man is an Island"),
-    new Qoutes(2, "Barack Obama", "Yes you you can"),
-    new Qoutes(3, "Joel Osteen", "Believe in you can do it!"),
-    new Qoutes(4, "Unknown", "Karma is a bitch"),
-    new Qoutes(5, "Activists", "You can people but you cannot finish ideas")
+    new Qoutes(1, "Martin Luther King", "No man is an Island",new Date(2020,3,14)),
+    new Qoutes(2, "Barack Obama", "Yes you you can",new Date(2019,7,14 )),
+    new Qoutes(3, "Joel Osteen", "Believe in you can do it!",new Date(2019,5,14 )),
+    new Qoutes(4, "Unknown", "Karma is a bitch",new Date(201,2,14 )),
+    new Qoutes(5, "Activists", "You can people but you cannot finish ideas",new Date(2019,2,10 )),
   ];
   toggleDetails(index) {
     this.qoutes[index].showDescription = !this.qoutes[index].showDescription;
